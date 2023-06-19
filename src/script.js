627947d7bd5a445d07b7615e3cc4717dff3a5eae
Tri <trimpham98@gmail.com>
2023-06-19 12:20:02 -0700

    Change particle size and count

--- src/script.js
+++ src/script.js
@@ -24,12 +24,21 @@ const textureLoader = new THREE.TextureLoader();
  */
 
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry(1, 32, 32);
+const count = 5000;
+
+const positions = new Float32Array(count * 3); // Multiply by 3 since each position has (x, y, z)
+
+for (let i = 0; i < count * 3; i++) { // Multiply by 3 since each position has (x, y, z)
+  positions[i] = (Math.random() - 0.5) * 10;
+}
+
+particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.02,
-  sizeAttenuation: true // add perspective based on distance from camera
+  size: 0.1,
+  sizeAttenuation: true, // add perspective based on distance from camera
 });
 
 // Points
